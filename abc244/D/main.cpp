238f6e1b0667182828fc69c7c118a2586f4870b2
cron <cron@cronnoMacBook-Pro.local>
2022-05-25 17:25:27 +0900

    Update abc244_D

--- abc244/D/main.cpp
+++ abc244/D/main.cpp
@@ -13,16 +13,35 @@ using vl = vector<long long>;
 #define debug(x)
 #endif
 template <typename ... Args>
-std::string format(const std::string& fmt, Args ... args )
-{
-    size_t len = std::snprintf( nullptr, 0, fmt.c_str(), args ... );
+std::string format(const std::string& fmt, Args ... args ) {
+    size_t len = std::snprintf(nullptr, 0, fmt.c_str(), args ...);
     std::vector<char> buf(len + 1);
-    std::snprintf(&buf[0], len + 1, fmt.c_str(), args ... );
+    std::snprintf(&buf[0], len + 1, fmt.c_str(), args ...);
     return std::string(&buf[0], &buf[0] + len);
 }
 
-void solve(std::vector<std::string> S, std::vector<std::string> T){
+std::string join(const std::vector<std::string>& v, const char* delim = 0) {
+    std::string s;
+    if (!v.empty()) {
+        s += v[0];
+        for (decltype(v.size()) i = 1, c = v.size(); i < c; ++i) {
+            if (delim) s += delim;
+            s += v[i];
+        }
+    }
+    return s;
+}
 
+void solve(std::vector<std::string> S, std::vector<std::string> T){
+    string SS = join(S, "");
+    string TT = join(T,"");
+    vector<string> A = {"RGB","GBR","BRG"};
+    if (std::count(A.begin(), A.end(), SS)==std::count(A.begin(), A.end(), TT)) {
+        cout << "Yes";
+    } else {
+        cout << "No";
+    }
+    cout << endl;
 }
 
 int main(){
