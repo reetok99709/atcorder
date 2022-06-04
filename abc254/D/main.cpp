ab113f58a0e81ef9c5cfd8f218ff37147e5c606c
cronree-91 <reetok99709@users.noreply.github.com>
2022-06-04 21:47:04 +0900

    Update abc254_d

--- abc254/D/main.cpp
+++ abc254/D/main.cpp
@@ -19,9 +19,48 @@ std::string format(const std::string& fmt, Args ... args )
     std::snprintf(&buf[0], len + 1, fmt.c_str(), args ... );
     return std::string(&buf[0], &buf[0] + len);
 }
+// 素因数分解
+template <typename T>
+map<T, T> prime_factor(T n) {
+    map<T, T> ret;
+    for (T i = 2; i * i <= n; i++) {
+        T tmp = 0;
+        while (n % i == 0) {
+            tmp++;
+            n /= i;
+        }
+        ret[i] = tmp;
+    }
+    if (n != 1) ret[n] = 1;
+    return ret;
+}
+// 約数の個数を求める
+template <typename T>
+T divisor_num(T N) {
+    map<T, T> pf = prime_factor(N);
+    T ret = 1;
+    for (auto p : pf) {
+        ret *= (p.second + 1);
+    }
+    return ret;
+}
+// 平方数かどうかの判定
+bool is_squere(long long N) {
+    return divisor_num(N) % 2 == 1;
+}
 
 void solve(long long N){
-
+    int ans = 0;
+    rep(ii, N) {
+        rep(jj, N) {
+            ll i = ii+1;
+            ll j = jj+1;
+            if (is_squere(i*j)) {
+                ans+=1;
+            }
+        }
+    }
+    cout << ans << endl;
 }
 
 int main(){
