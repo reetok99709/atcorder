f340cda3a19d149f29a65f5c8ac595a2a5679e78
cronree-91 <reetok99709@users.noreply.github.com>
2022-06-15 17:39:18 +0900

    Update abc234_c

---
+++ abc234/C/main.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+#define setfixed(x) std::fixed << std::setprecision(x)
+#define setscientific(x) std::scientific << std::setprecision(x)
+using ll = long long;
+using vl = vector<long long>;
+#ifdef _DEBUG
+#define debug(x) cerr << "[DEBUG] " << __LINE__ << ": " << #x << " = " << x << endl
+#else
+#define debug(x)
+#endif
+template <typename ... Args>
+std::string format(const std::string& fmt, Args ... args )
+{
+    size_t len = std::snprintf( nullptr, 0, fmt.c_str(), args ... );
+    std::vector<char> buf(len + 1);
+    std::snprintf(&buf[0], len + 1, fmt.c_str(), args ... );
+    return std::string(&buf[0], &buf[0] + len);
+}
+template<typename T>
+void output_vec(vector<T> vec) {
+    int size = vec.size();
+    for (int i=0; i < size; i++) {
+        cout << vec.at(i);
+        if (i+1==size) {
+            cout << endl;
+        } else {
+            cout << " ";
+        }
+    }
+}
+template<typename T>
+void output_vec(vector<vector<T>> vec) {
+    int size = vec.size();
+    for (int i=0; i < size; i++) {
+        output_vec(vec.at(i));
+    }
+}
+
+string toBinary(ll n)
+{
+    string r;
+    while (n != 0){
+        r += ( n % 2 == 0 ? "0" : "1" );
+        n /= 2;
+    }
+    return r;
+}
+
+void solve(long long K) {
+    cout << regex_replace(toBinary(K), regex("1"), "2") << endl;
+}
+
+int main(){
+    long long K;
+    std::scanf("%lld", &K);
+    solve(K);
+    return 0;
+}
